8beebbb06caff1115aa53756faa52917cf6b0a75
Phat Thaveepholcharoen <overtone00445@gmail.com>
2023-05-11 21:53:45 +0700

    feat: setup absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ReactComponent as ReactIcon } from "@/assets/react.svg";
+import Test from "@/components/Test";
+
 function App() {
-  return <div className="container p-10">React Typescript Template</div>;
+  return (
+    <div className="container p-10">
+      <Test />
+      <ReactIcon width={40} height={40} />
+      <div className="text-blue-800 font-bold text-xl">
+        React Typescript Template
+      </div>
+    </div>
+  );
 }
 
 export default App;
