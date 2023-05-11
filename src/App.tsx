70611a68b2285c6ccb5ce401832af9eabfcc9033
Phat Thaveepholcharoen <overtone00445@gmail.com>
2023-05-11 21:39:58 +0700

    feat: init new structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import React from "react";
-import "./index.css";
-
 function App() {
-  return (
-    <div className="bg-blue-300">
-      <h1 className="text-3xl font-bold underline text-center">
-        Start coding!
-      </h1>
-    </div>
-  );
+  return <div className="container p-10">React Typescript Template</div>;
 }
 
 export default App;
